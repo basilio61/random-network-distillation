164860f0554eaaf19c0a93c1b5ebffbc01df4123
basilio61 <41764898+basilio61@users.noreply.github.com>
2019-11-13 19:55:45 -0300

    Update run_atari.py

--- run_atari.py
+++ run_atari.py
@@ -10,15 +10,17 @@ from cmd_util import make_atari_env, arg_parser
 from policies.cnn_gru_policy_dynamics import CnnGruPolicy
 from policies.cnn_policy_param_matched import CnnPolicy
 from ppo_agent import PpoAgent
+from ppo_agent_light import PpoAgentLight
 from utils import set_global_seeds
 from vec_env import VecFrameStack
+from atari_wrappers import make_atari
 
 
-def train(*, env_id, num_env, hps, num_timesteps, seed):
+def train(*, env_id, num_env, hps, num_timesteps, seed, save_freq, retrain, test, render):
     venv = VecFrameStack(
         make_atari_env(env_id, num_env, seed, wrapper_kwargs=dict(),
                        start_index=num_env * MPI.COMM_WORLD.Get_rank(),
-                       max_episode_steps=hps.pop('max_episode_steps')),
+                       max_episode_steps=hps.pop('max_episode_steps'), ifrender=render),
         hps.pop('frame_stack'))
     # venv.score_multiple = {'Mario': 500,
     #                        'MontezumaRevengeNoFrameskip-v4': 100,
@@ -32,9 +34,11 @@ def train(*, env_id, num_env, hps, num_timesteps, seed):
     venv.record_obs = True if env_id == 'SolarisNoFrameskip-v4' else False
     ob_space = venv.observation_space
     ac_space = venv.action_space
+
     gamma = hps.pop('gamma')
     policy = {'rnn': CnnGruPolicy,
               'cnn': CnnPolicy}[hps.pop('policy')]
+
     agent = PpoAgent(
         scope='ppo',
         ob_space=ob_space,
@@ -63,23 +67,90 @@ def train(*, env_id, num_env, hps, num_timesteps, seed):
         update_ob_stats_every_step=hps.pop('update_ob_stats_every_step'),
         int_coeff=hps.pop('int_coeff'),
         ext_coeff=hps.pop('ext_coeff'),
+        retrain=retrain,
+        test=test,
+        ifrender=render,
     )
     agent.start_interaction([venv])
-    if hps.pop('update_ob_stats_from_random_agent'):
+    if hps.pop('update_ob_stats_from_random_agent') and not test:
         agent.collect_random_statistics(num_timesteps=128*50)
     assert len(hps) == 0, "Unused hyperparameters: %s" % list(hps.keys())
 
     counter = 0
     while True:
+        # print("global time:{}".format(counter))
         info = agent.step()
         if info['update']:
             logger.logkvs(info['update'])
             logger.dumpkvs()
-            counter += 1
+        counter += 1
+        if agent.I.step_count % save_freq == 0 and agent.I.step_count > 0 and not test:   # 测试时不存模型
+            agent.save_model(agent.I.step_count)
+
         if agent.I.stats['tcount'] > num_timesteps:
             break
 
     agent.stop_interaction()
+
+
+# def test(*, env_id, hps, seed, test, render=True, num_env=1):
+#     max_steps = hps.pop('max_episode_steps')
+#     test_env = make_atari(env_id, max_steps)
+#     test_env.seed(seed)
+#
+#     test_env.score_multiple = 1
+#     test_env.record_obs = False
+#     ob_space = test_env.observation_space
+#     ac_space = test_env.action_space
+#
+#     gamma = hps.pop('gamma')
+#     policy = {'rnn': CnnGruPolicy,
+#               'cnn': CnnPolicy}[hps.pop('policy')]
+#
+#     agent = PpoAgentLight(
+#         scope='ppo',
+#         env=test_env,
+#         ob_space=ob_space,
+#         ac_space=ac_space,
+#         stochpol_fn=functools.partial(
+#             policy,
+#             scope='pol',
+#             ob_space=ob_space,
+#             ac_space=ac_space,
+#             update_ob_stats_independently_per_gpu=hps.pop('update_ob_stats_independently_per_gpu'),
+#             proportion_of_exp_used_for_predictor_update=hps.pop('proportion_of_exp_used_for_predictor_update'),
+#             dynamics_bonus=hps.pop("dynamics_bonus")
+#         ),
+#         gamma=gamma,
+#         gamma_ext=hps.pop('gamma_ext'),
+#         lam=hps.pop('lam'),
+#         nepochs=hps.pop('nepochs'),
+#         nminibatches=hps.pop('nminibatches'),
+#         lr=hps.pop('lr'),
+#         cliprange=0.1,
+#         nsteps=128,
+#         ent_coef=0.001,
+#         max_grad_norm=hps.pop('max_grad_norm'),
+#         use_news=hps.pop("use_news"),
+#         update_ob_stats_every_step=hps.pop('update_ob_stats_every_step'),
+#         int_coeff=hps.pop('int_coeff'),
+#         ext_coeff=hps.pop('ext_coeff'),
+#         test=test,
+#         render=render,
+#     )
+#
+#     for i in range(max_steps):
+#         while True:
+#             ob, reward, done, ale = agent.step()
+#
+#             if done or agent.step_count > hps.pop('max_episode_steps'):
+#                 print("episode:{}, steps:{}, reward:{}".format(agent.episode_count, agent.step_count, reward))
+#                 agent.reset()
+#                 break
+#     print("finish!")
+
+
+
 
 
 def add_env_params(parser):
@@ -106,6 +177,10 @@ def main():
     parser.add_argument('--int_coeff', type=float, default=1.)
     parser.add_argument('--ext_coeff', type=float, default=2.)
     parser.add_argument('--dynamics_bonus', type=int, default=0)
+    parser.add_argument("--save_freq", type=int, default=10)
+    parser.add_argument("--retrain", type=bool, default=True)   # 重新训练不用存的数据
+    parser.add_argument("--test", type=bool, default=False)     # True 不训练，只测试
+    parser.add_argument("--render", type=bool, default=False)   # 不显示
 
 
     args = parser.parse_args()
@@ -115,7 +190,7 @@ def main():
             f.write(args.tag)
         # shutil.copytree(os.path.dirname(os.path.abspath(__file__)), os.path.join(logger.get_dir(), 'code'))
 
-    mpi_util.setup_mpi_gpus()
+    #mpi_util.setup_mpi_gpus()
 
     seed = 10000 * args.seed + MPI.COMM_WORLD.Get_rank()
     set_global_seeds(seed)
@@ -142,8 +217,10 @@ def main():
     )
 
     tf_util.make_session(make_default=True)
+    print("retrain:{}, test:{}, render:{}".format(args.retrain, args.test, args.render))
+
     train(env_id=args.env, num_env=args.num_env, seed=seed,
-        num_timesteps=args.num_timesteps, hps=hps)
+            num_timesteps=args.num_timesteps, hps=hps, save_freq=args.save_freq, retrain=args.retrain, test=args.test, render=args.render)
 
 
 if __name__ == '__main__':
